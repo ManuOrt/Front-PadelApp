cde192405ff0b5f34bdb8fbd5a3ffa6594661734
Manu Ortega <mortega@indigenasdigitales.com>
2024-02-09 21:51:54 +0100

    Adding the video on the onboarding and setting the buttons

--- lib/screens/onboarding/onboarding_page_model.dart
+++ lib/screens/onboarding/onboarding_page_model.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:front_end_padelapp/utils/app_colors.dart';
-import 'package:front_end_padelapp/widgets/ball_indicator.dart';
+
+import '../../widgets/widgets.dart';
 
 class OnboardingPageModel {
   final String title;
-  final String description;
+  String? description;
   String? imagePath;
+  String? videoPath;
   int? pageCount;
 
   OnboardingPageModel({
     required this.title,
-    required this.description,
+    this.description,
     this.imagePath,
+    this.videoPath,
     this.pageCount,
   });
 
@@ -19,6 +22,7 @@ class OnboardingPageModel {
     required int currentIndex,
     required VoidCallback onNextPressed,
     required VoidCallback onBackPressed,
+    required VoidCallback onFinalButtonPressed,
   }) {
     return Material(
       child: DecoratedBox(
@@ -33,80 +37,110 @@ class OnboardingPageModel {
           ),
         ),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 200),
-                child: Text(
-                  title,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 200),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 40.0),
-              Container(
-                padding: const EdgeInsets.all(25.0),
-                child: Text(
-                  description,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w300,
-                    height: 1.8,
-                    fontSize: 20.0,
-                  ),
-                  textAlign: TextAlign.center,
+                const SizedBox(height: 40.0),
+                Container(
+                  padding: const EdgeInsets.all(25.0),
+                  child: description != null && description!.isNotEmpty
+                      ? Text(
+                          description!,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            height: 1.8,
+                            fontSize: 20.0,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      : VideoYoutubePlayerWidget(
+                          videoId: videoPath!,
+                        ),
                 ),
-              ),
-              const SizedBox(height: 10.0),
-              BallIndicator(
-                  pageCount: pageCount ??= 3, currentIndex: currentIndex),
-              const SizedBox(height: 50.0),
-              ElevatedButton(
-                onPressed: onNextPressed,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primary,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 120.0,
-                    vertical: 12.0,
+                const SizedBox(height: 10.0),
+                BallIndicator(
+                    pageCount: pageCount ??= 4, currentIndex: currentIndex),
+                const SizedBox(height: 50.0),
+                if (currentIndex < pageCount! - 1)
+                  ElevatedButton(
+                    onPressed: onNextPressed,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 120.0,
+                        vertical: 12.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'SIGUIENTE',
+                      style: TextStyle(
+                        color: AppColors.primaryWhite,
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(14.0),
+                const SizedBox(height: 10.0),
+                if (currentIndex == pageCount! - 1)
+                  ElevatedButton(
+                    onPressed: onFinalButtonPressed,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 85.0,
+                        vertical: 12.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'IR A LA APLICACIÓN',
+                      style: TextStyle(
+                        color: AppColors.primaryWhite,
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
-                ),
-                child: const Text(
-                  'SIGUIENTE',
-                  style: TextStyle(
-                    color: AppColors.primaryWhite,
-                    fontSize: 16.0,
+                const SizedBox(height: 10.0),
+                if (currentIndex > 0)
+                  ElevatedButton(
+                    onPressed: onBackPressed,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 135.0,
+                        vertical: 12.0,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14.0),
+                      ),
+                    ),
+                    child: const Text(
+                      'ATRÁS',
+                      style: TextStyle(
+                        color: AppColors.primaryWhite,
+                        fontSize: 16.0,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              const SizedBox(height: 10.0),
-              ElevatedButton(
-                onPressed: onBackPressed,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColors.primary,
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 135.0,
-                    vertical: 12.0,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(14.0),
-                  ),
-                ),
-                child: const Text(
-                  'ATRÁS',
-                  style: TextStyle(
-                    color: AppColors.primaryWhite,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
