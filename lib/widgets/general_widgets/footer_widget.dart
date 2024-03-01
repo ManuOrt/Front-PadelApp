e9e2fb5d124eda91f65e4ee1851c3485f6bf47e8
FJMudoy <franciscojanermudoy@gmail.com>
2024-03-01 19:39:25 +0100

    Menu change

--- lib/widgets/general_widgets/footer_widget.dart
+++ lib/widgets/general_widgets/footer_widget.dart
@@ -1,20 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:front_end_padelapp/screens/home/home_screen.dart';
 import 'package:front_end_padelapp/utils/app_colors.dart';
 
-class FooterWidget extends StatelessWidget {
-  const FooterWidget({super.key, required this.size});
+class FooterWidget extends StatefulWidget {
+  const FooterWidget({Key? key, required this.size}) : super(key: key);
 
   final Size size;
 
   @override
+  _FooterWidgetState createState() => _FooterWidgetState();
+}
+
+class _FooterWidgetState extends State<FooterWidget> {
+  int selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      height: size.height * 0.07,
+      height: widget.size.height * 0.07,
       decoration: BoxDecoration(
-        borderRadius: const BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-        ),
         color: AppColors.primary.withOpacity(0.9),
       ),
       child: Row(
@@ -26,34 +30,57 @@ class FooterWidget extends StatelessWidget {
               color: AppColors.primaryWhite,
             ),
             onPressed: () {
-              Navigator.pushNamed(context, 'home');
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
             },
           ),
+          const VerticalDivider(color: AppColors.primaryWhite, width: 1),
           IconButton(
             icon: const Icon(
               Icons.favorite,
               color: AppColors.primaryWhite,
             ),
             onPressed: () {
-              //TODO implement favorite screen
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
             },
           ),
+          const VerticalDivider(color: AppColors.primaryWhite, width: 1),
           IconButton(
             icon: const Icon(
               Icons.calendar_month,
               color: AppColors.primaryWhite,
             ),
             onPressed: () {
-              //TODO implement calendar screen
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
             },
           ),
+          const VerticalDivider(color: AppColors.primaryWhite, width: 1),
           IconButton(
             icon: const Icon(
-              Icons.chat_rounded,
+              Icons.chat,
               color: AppColors.primaryWhite,
             ),
             onPressed: () {
-              //TODO implement chat screen
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const HomeScreen(),
+                ),
+              );
             },
           ),
         ],
