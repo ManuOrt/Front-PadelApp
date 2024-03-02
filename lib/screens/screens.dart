f09e6bc413779b4b39ae8b8829c20ad92edb12e6
Manu Ortega <mortega@indigenasdigitales.com>
2024-03-02 12:26:27 +0100

    fix - fixing conflicts

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -4,3 +4,5 @@ export 'package:front_end_padelapp/screens/auth/recovery_password_screen.dart';
 export 'package:front_end_padelapp/screens/auth/register_screen.dart';
 export 'package:front_end_padelapp/screens/home/home_screen.dart';
 export 'package:front_end_padelapp/screens/onboarding/onboarding_screens.dart';
+export 'package:front_end_padelapp/screens/settings/options_screen.dart';
+export 'package:front_end_padelapp/screens/settings/profile_screen.dart';
